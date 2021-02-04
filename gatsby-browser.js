43b40a28e5ffc2e1f4eaf5d40b953136b18aaae7
Dominik 'Dragenet' Domagalski <dragenetdev@gmail.com>
2021-02-04 12:41:16 +0100

    Create new README and clean up

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-// import React from 'react'
-// import VirtualViewport from './src/layouts/VirtualViewport'
-
-// export const wrapRootElement = ({ element }) => (
-//   <VirtualViewport>{element}</VirtualViewport>
-// )
