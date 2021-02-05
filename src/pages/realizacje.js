b4b84b15ebf956053b76a1c00be4669ecfab21ca
Dominik 'Dragenet' Domagalski <dragenetdev@gmail.com>
2021-02-05 18:43:53 +0100

    Add realization card rendering base on data

--- src/pages/realizacje.js
+++ src/pages/realizacje.js
@@ -8,12 +8,15 @@ import RealizationCard, { Title } from '../components/RealizationCard'
 const Realizacje = ({ data }) => {
   return (
     <ImageGrid gap="20px">
-      <RealizationCard
-        image={data.file.childImageSharp.fluid}
-        link="/realizacje/kuchnia"
-      >
-        <Title>Kuchnia Jarosława z Zadupowic Mniejszych Duchownych</Title>
-      </RealizationCard>
+      {data.allDatoCmsRealization.nodes.map(node => (
+        <RealizationCard
+          key={node.slug}
+          image={node.heroImage.fluid}
+          link={node.slug}
+        >
+          <Title>{node.title}</Title>
+        </RealizationCard>
+      ))}
     </ImageGrid>
   )
 }
@@ -23,11 +26,15 @@ Realizacje.propTypes = {}
 Realizacje.defaultProps = {}
 
 export const query = graphql`
-  query MyQuery {
-    file(relativePath: { eq: "card1.jpg" }) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid_tracedSVG
+  query getRealizationsQuery {
+    allDatoCmsRealization {
+      nodes {
+        title
+        slug
+        heroImage {
+          fluid {
+            ...GatsbyDatoCmsFluid_tracedSVG
+          }
         }
       }
     }
